cc407b6ff64eab4c08c33d84492f4645665ec09d
IT <spanyukhin@list.ru>
2023-10-18 20:58:34 +0300

    the frist and the second taks are added

--- task19/Program.cs
+++ task19/Program.cs
@@ -1,2 +1,76 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+// void IsPalindrome(int number){
+//         int h = number;
+//         int i = 1;
+//         while(number / 10 != 0){
+//             number = number / 10;
+//             i++;
+//         }
+//         System.Console.WriteLine($"{i}");
+//         if (i < 5 || i > 5){
+//             System.Console.WriteLine("Число не пятизначное!");
+//         }
+//         else{
+//             for(int j = 10; j <=100; j=j*10){
+//                 int x = h % (1000*j);
+//                 x = x / (100*j);
+//                 int y = (h*10) % (10000/j);
+//                 y = y /(1000/j);
+//                 if (x != y){
+//                     System.Console.WriteLine("Число не является палиндромом.");
+//                 }
+//             }
+//             System.Console.WriteLine("Число является палиндромом.");
+//         } 
+// }
+
+//using System;
+using System;
+
+public class Answer
+{
+    static bool IsPalindrome(int number){
+      // Введите свое решение ниже
+      int h = number;
+        int i = 1;
+        while(number / 10 != 0){
+            number = number / 10;
+            i++;
+        }
+        System.Console.WriteLine($"{i}");
+        if (i < 5 || i > 5){
+            System.Console.WriteLine("Число не пятизначное!");
+            return false;
+        }
+        else{
+            for(int j = 10; j <=100; j=j*10){
+                int x = h % (1000*j);
+                x = x / (100*j);
+                int y = (h*10) % (10000/j);
+                y = y /(1000/j);
+                if (x != y){
+                    System.Console.WriteLine("Число не является палиндромом.");
+                    return false;
+                }
+            }
+            System.Console.WriteLine("Число является палиндромом.");
+            return true;
+        }
+    }
+  
+  // Не удаляйте и не меняйте метод Main! 
+      static public void Main(string[] args) {
+        int number;
+
+        if (args.Length >= 1) {
+            number = int.Parse(args[0]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            number = 64546;
+        }
+
+        // Не удаляйте строки ниже
+        bool result = IsPalindrome(number);
+        System.Console.WriteLine($"{result}");
+    }
+}
---
+++ task21/Program.cs
@@ -0,0 +1,59 @@
+﻿// using System;
+//   // Не удаляйте и не меняйте метод Main! 
+// public static void Main(string[] args) {
+//         int x1, x2, x3, y1, y2, y3;
+
+//         if (args.Length >= 6) {
+//             x1 = int.Parse(args[0]);
+//             x2 = int.Parse(args[1]);
+//             x3 = int.Parse(args[2]);
+//             y1 = int.Parse(args[3]);
+//             y2 = int.Parse(args[4]);
+//             y3 = int.Parse(args[5]);
+//         } else {
+//            // Здесь вы можете поменять значения для отправки кода на Выполнение
+//             x1 = 3;
+//             x2 = 6;
+//             x3 = 8;
+//             y1 = 2;
+//             y2 = 1;
+//             y3 = -7;
+//         }
+
+//         // Не удаляйте строки ниже
+//         double result = Length(new int[]{x1, x2, x3}, new int[]{y1, y2, y3});
+//         Console.WriteLine($"{result:F2}");
+//     }
+// public class Answer
+// {
+//     private static double Length(int[] pointA, int[] pointB)
+//     {
+//       // Введите свое решение ниже
+//         double total = 0;
+//        int[] pointA = [x1, x2, x3];
+//        int[] pointB = [y1, y2, y3];
+
+//         for (int i = 0; i < 2; i++){
+//         double sqr = Math.Pow(pointB[i] - pointA[i], 2);
+//         total = total + sqr;
+//         }
+//         double root = Math.Sqrt(sqr);
+//         return root;
+//     }
+
+//     }
+int[] pointA1 = {3, 6, 8};
+int[] pointB1 = {2, 1, -7};
+double Length(int[] pointA, int[] pointB)
+    {
+        double total = 0;
+        for (int i = 0; i <= 2; i++){
+        
+        double sq = Math.Pow(pointB1[i] - pointA1[i], 2);
+        total = total + sq;
+        }
+        double root = Math.Sqrt(total);
+        Console.WriteLine($"Расстояние между точками в пространстве: {root}");
+        return root;
+    }
+Length(pointA1, pointB1);
